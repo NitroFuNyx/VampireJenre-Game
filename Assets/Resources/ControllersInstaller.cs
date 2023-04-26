47523cc186021e890dbf22521b5d0c5cbce89c42
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-04-26 15:25:37 +0300

    Spawn enemies beyond map system added

--- Assets/Resources/ControllersInstaller.cs
+++ Assets/Resources/ControllersInstaller.cs
@@ -6,9 +6,11 @@ public class ControllersInstaller : MonoInstaller
     [Header("References")]
     [Space]
     [SerializeField] private DataPersistanceManager dataPersistanceManager;
+    [SerializeField] private PoolItemsManager poolItemsManager;
 
     public override void InstallBindings()
     {
         Container.Bind<DataPersistanceManager>().FromInstance(dataPersistanceManager).AsSingle().NonLazy();
+        Container.Bind<PoolItemsManager>().FromInstance(poolItemsManager).AsSingle().NonLazy();
     }
 }
---
+++ Assets/Scripts/Spawn Enemies/EnemySpawner.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Zenject;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Holders")]
+    [Space]
+    [SerializeField] private Transform spawnedObjectsHolder;
+    [Header("Spawn Positions")]
+    [Space]
+    [SerializeField] private List<Transform> spawnPositionsList = new List<Transform>();
+
+    private PoolItemsManager _poolItemsManager;
+
+    [SerializeField] private List<Transform> takenSpawnPositionsList = new List<Transform>();
+
+    private float resetSpawnPositionsDelay = 1f;
+
+    #region Zenject
+    [Inject]
+    private void Construct(PoolItemsManager poolItemsManager)
+    {
+        _poolItemsManager = poolItemsManager;
+    }
+    #endregion Zenject
+
+    [ContextMenu("Spawn")]
+    public void Spawn()
+    {
+        SpawnEnemyWave(PoolItemsTypes.Enemy, 200);
+    }
+
+    public void SpawnEnemyWave(PoolItemsTypes enemyType, int enemiesAmount)
+    {
+        if(enemiesAmount > spawnPositionsList.Count)
+        {
+            enemiesAmount = spawnPositionsList.Count;
+        }
+
+        for(int i = 0; i < enemiesAmount; i++)
+        {
+            SpawnEnemy();
+        }
+
+        StartCoroutine(ResetSpawnPositionsCoroutine());
+    }
+
+    public void SpawnEnemy()
+    {
+        Transform spawnPoint = GetRandomSpawnPoint();
+        _poolItemsManager.SpawnItemFromPool(PoolItemsTypes.Enemy, spawnPoint.position, spawnPoint.rotation, spawnedObjectsHolder);
+    }
+
+    private Transform GetRandomSpawnPoint()
+    {
+        Transform spawnPoint = spawnPositionsList[0];
+
+        int randomPointIndex = Random.Range(0, spawnPositionsList.Count);
+
+        spawnPoint = spawnPositionsList[randomPointIndex];
+
+        takenSpawnPositionsList.Add(spawnPoint);
+        spawnPositionsList.Remove(spawnPoint);
+
+        return spawnPoint;
+    }
+
+    private IEnumerator ResetSpawnPositionsCoroutine()
+    {
+        yield return new WaitForSeconds(resetSpawnPositionsDelay);
+
+        for(int i = 0; i < takenSpawnPositionsList.Count; i++)
+        {
+            if(!spawnPositionsList.Contains(takenSpawnPositionsList[i]))
+            {
+                spawnPositionsList.Add(takenSpawnPositionsList[i]);
+            }
+        }
+
+        takenSpawnPositionsList.Clear();
+    }
+}
