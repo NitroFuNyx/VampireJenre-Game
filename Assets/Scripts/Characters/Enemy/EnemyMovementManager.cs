33ec1fce085b0c45a2e9da4abb02bd3058fb0dd3
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-04-27 16:58:06 +0300

    Enemy movement updated

--- Assets/Scripts/Characters/Enemy/EnemyMovementManager.cs
+++ Assets/Scripts/Characters/Enemy/EnemyMovementManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections;
 using DG.Tweening;
 
 public class EnemyMovementManager : MonoBehaviour
 {
+    [Header("Move Data")]
+    [Space]
+    [SerializeField] private float moveVelocity = 10f;
+    [SerializeField] private float rotationVelocity = 10f;
+
     private PlayerController player;
+
+    private bool canMove = false;
 
     private void Awake()
     {
@@ -12,7 +20,29 @@ public class EnemyMovementManager : MonoBehaviour
 
     public void MoveToPlayer()
     {
-        Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-        transform.DOMove(playerPos, 10f);
+        canMove = true;
+        //Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        //transform.DOMove(playerPos, 10f);
+        StartCoroutine(MoveToPlayerCoroutine());
+    }
+
+    private IEnumerator MoveToPlayerCoroutine()
+    {
+        if (canMove)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 originalPlayerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            float travelPercent = 0f;
+
+            while (Vector3.Distance(transform.position, originalPlayerPos) > 0.1f)
+            {
+                Vector3 targetPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+                travelPercent += moveVelocity * Time.deltaTime;
+                transform.position = Vector3.Lerp(startPos, targetPos, travelPercent);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetPos - transform.position), rotationVelocity * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
