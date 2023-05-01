ec0e6b65e69981e2fcd0b7d56e52d6c38923c085
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-05-01 13:36:03 +0300

    Scene updated

--- Assets/Scripts/Spawn Enemies/SpawnEnemiesManager.cs
+++ Assets/Scripts/Spawn Enemies/SpawnEnemiesManager.cs
@@ -13,7 +13,7 @@ public class SpawnEnemiesManager : MonoBehaviour
     [ContextMenu("Spawn Beyond Map")]
     public void SpawnEnemiesBeyondMap()
     {
-        spawner_BeyondMap.SpawnEnemyWave(PoolItemsTypes.Enemy_Ghost, 100);
+        spawner_BeyondMap.SpawnEnemyWave(PoolItemsTypes.Enemy_Ghost, 40);
     }
 
     [ContextMenu("Spawn On Map")]
