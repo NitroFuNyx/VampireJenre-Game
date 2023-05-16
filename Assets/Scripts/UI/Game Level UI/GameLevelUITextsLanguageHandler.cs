4f879ac11d9736ca5a1e05afc40c54737201a776
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-05-16 12:39:36 +0300

    Skills background added

--- Assets/Scripts/UI/Game Level UI/GameLevelUITextsLanguageHandler.cs
+++ Assets/Scripts/UI/Game Level UI/GameLevelUITextsLanguageHandler.cs
@@ -27,20 +27,20 @@ public class GameLevelUITextsLanguageHandler : TextsLanguageUpdateHandler
 
     public override void OnLanguageChange_ExecuteReaction(LanguageTextsHolder languageHolder)
     {
-        pausePanelTitleText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.panelTitleText;
-        pausePanelDescribtionText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.panelDescribtionText;
-        resumeButtonText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.resumeButtonText;
+        //pausePanelTitleText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.panelTitleText;
+        //pausePanelDescribtionText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.panelDescribtionText;
+        //resumeButtonText.text = languageHolder.data.gameLevelUITexts.pausePanelTexts.resumeButtonText;
 
-        levelUpgradePanelTitleText.text = languageHolder.data.gameLevelUITexts.levelUpgradePanelTexts.panelTitleText;
-        levelUpgradePanelDescribtionText.text = languageHolder.data.gameLevelUITexts.levelUpgradePanelTexts.panelDescribtionText;
+        //levelUpgradePanelTitleText.text = languageHolder.data.gameLevelUITexts.levelUpgradePanelTexts.panelTitleText;
+        //levelUpgradePanelDescribtionText.text = languageHolder.data.gameLevelUITexts.levelUpgradePanelTexts.panelDescribtionText;
 
-        multiplyButtonText.text = languageHolder.data.gameLevelUITexts.victoryPanelTexts.multiplyButtonText;
+        //multiplyButtonText.text = languageHolder.data.gameLevelUITexts.victoryPanelTexts.multiplyButtonText;
 
-        getUpButtonText.text = languageHolder.data.gameLevelUITexts.defeatPanelTexts.getUpButtonText;
+        //getUpButtonText.text = languageHolder.data.gameLevelUITexts.defeatPanelTexts.getUpButtonText;
 
-        for(int i = 0; i < menuButtonsTextsList.Count; i++)
-        {
-            menuButtonsTextsList[i].text = languageHolder.data.gameLevelUITexts.menuButtonText;
-        }
+        //for(int i = 0; i < menuButtonsTextsList.Count; i++)
+        //{
+        //    menuButtonsTextsList[i].text = languageHolder.data.gameLevelUITexts.menuButtonText;
+        //}
     }
 }
