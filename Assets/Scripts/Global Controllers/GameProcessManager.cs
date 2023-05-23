951040fce1ee479c34570d44e458977af6452e68
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-05-23 16:12:06 +0300

    Scene merged

--- Assets/Scripts/Global Controllers/GameProcessManager.cs
+++ Assets/Scripts/Global Controllers/GameProcessManager.cs
@@ -7,7 +7,6 @@ public class GameProcessManager : MonoBehaviour
 {
     [Header("Player")]
     [Space]
-   // [SerializeField] private GameObject skillObject;
     [SerializeField] private PlayerComponentsManager player;
     [Header("Map Progreess Data")]
     [Space]
@@ -59,7 +58,6 @@ public class GameProcessManager : MonoBehaviour
 
     public void GameLost_ExecuteReaction()
     {
-       // skillObject.SetActive(false);
         OnPlayerLost?.Invoke();
         ResetMapProgress();
     }
@@ -73,7 +71,6 @@ public class GameProcessManager : MonoBehaviour
     private IEnumerator StartGameCoroutine()
     {
         yield return new WaitForSeconds(2f);
-       // skillObject.SetActive(true);
         _spawnEnemiesManager.SpawnEnemies();
     }
 }
