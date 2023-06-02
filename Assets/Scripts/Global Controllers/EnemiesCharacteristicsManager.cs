6723dd559aed99841f144dc521e63bbb6c23641b
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-06-02 17:08:41 +0300

    Enmies start data changed

--- Assets/Scripts/Global Controllers/EnemiesCharacteristicsManager.cs
+++ Assets/Scripts/Global Controllers/EnemiesCharacteristicsManager.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class EnemiesCharacteristicsManager : MonoBehaviour
 {
-    
+    [Header("Enemies Current Data")]
+    [Space]
+    [SerializeField] private EnemyDataStruct currentEnemiesData;
+    [Header("Enemies Start Data")]
+    [Space]
+    [SerializeField] private EnemyStartDataSO enemyStartDataSO;
+
+    private PlayerExperienceManager _playerExperienceManager;
+
+    private void Awake()
+    {
+        currentEnemiesData = enemyStartDataSO.EnemyStartData;
+    }
+
+    #region Zenject
+    [Inject]
+    private void Construct(PlayerExperienceManager playerExperienceManager)
+    {
+        _playerExperienceManager = playerExperienceManager;
+    }
+    #endregion Zenject
 }
