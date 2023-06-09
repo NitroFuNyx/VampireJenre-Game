2a8c1e5970babafe3c554c2a96bd2ecd951a8f8f
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-06-09 17:27:00 +0300

    VFX audio sources fixed

--- Assets/Scripts/Characters/Enemy/EnemyComponentsManager.cs
+++ Assets/Scripts/Characters/Enemy/EnemyComponentsManager.cs
@@ -107,7 +107,7 @@ public class EnemyComponentsManager : EnemyBehaviour
         movementManager.StopMoving();
         animationsManager.SetAnimation_Die();
         getHitVfx.Play();
-        PoolItem bloodPuddleVfx = poolItemComponent.PoolItemsManager.SpawnItemFromPool(PoolItemsTypes.BloodPuddleVFX, bloodPuddleVfxPos.position,
+        PoolItem bloodPuddleVfx = poolItemComponent.PoolItemsManager.SpawnItemFromPool(PoolItemsTypes.BloodPuddleVFX, transform.position,
                                                                                        Quaternion.identity, null);
         bloodPuddleVfx.SetObjectAwakeState();
         poolItemComponent.SpawnEnemiesManager.RemoveEnemyFronOnMapList(this);
