5a72a4aba07dfd1f5f2ee97d91ba9301fc95c4ab
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-06-15 17:50:36 +0300

    Scene updated

--- Assets/Scripts/Advertisement/AdsRewardedSetup.cs
+++ Assets/Scripts/Advertisement/AdsRewardedSetup.cs
@@ -21,8 +21,8 @@ public class AdsRewardedSetup : MonoBehaviour
         
         MobileAds.Initialize((InitializationStatus initStatus) =>
         {
-            RegisterEventHandlers(rewardedAd);
             LoadRewardedAd();
+            RegisterEventHandlers(rewardedAd);
         });
     }
 
