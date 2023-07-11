ad9d1e3a3020ff75b08d79af22f538307aebfc98
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-07-11 17:28:25 +0300

    Deathmatch button added

---
+++ Assets/Scripts/UI/Game Level UI/Deathmatch Panel/PlayDeathmatchButton.cs
@@ -0,0 +1,20 @@
+using Zenject;
+
+public class PlayDeathmatchButton : ButtonInteractionHandler
+{
+    private MainUI _mainUI;
+
+    #region Zenject
+    [Inject]
+    private void Construct(MainUI mainUI)
+    {
+        _mainUI = mainUI;
+    }
+    #endregion Zenject
+
+    public override void ButtonActivated()
+    {
+        ShowAnimation_ButtonPressed();
+        _mainUI.DeathmatchButtonPressed_ExecuteReaction();
+    }
+}
