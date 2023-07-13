155aee72ce31f797d423478933bb21f4f3378195
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-07-13 17:51:43 +0300

    Animation states for deathmatch button added

--- Assets/Scripts/UI/Game Level UI/Deathmatch Panel/PlayDeathmatchButton.cs
+++ Assets/Scripts/UI/Game Level UI/Deathmatch Panel/PlayDeathmatchButton.cs
@@ -1,8 +1,19 @@
+using UnityEngine;
+using DG.Tweening;
 using Zenject;
 
 public class PlayDeathmatchButton : ButtonInteractionHandler
 {
+    [Header("Process Canceled Animation Data")]
+    [Space]
+    [SerializeField] private Vector3 scaleAnimationPunchVector = new Vector3(0.2f, 0.2f, 0.2f);
+    [SerializeField] private int scaleAnimationFrequency = 2;
+    [SerializeField] private float scaleAnimationDuration = 0.5f;
+    [SerializeField] private Color disabledColor;
+
     private DeathmatchAccessManager _deathmatchAccessManager;
+
+    private bool buttonActivated = false;
 
     #region Zenject
     [Inject]
@@ -14,11 +25,28 @@ public class PlayDeathmatchButton : ButtonInteractionHandler
 
     public override void ButtonActivated()
     {
-        _deathmatchAccessManager.PlayDeathmatchButtonPressed_ExecuteReaction(ShowAnimation_ButtonPressed, ShowFailedBuyingResult);
+        if(!buttonActivated)
+        {
+            buttonActivated = true;
+            _deathmatchAccessManager.PlayDeathmatchButtonPressed_ExecuteReaction(ShowSuccessfullBuyingResult, ShowFailedBuyingResult);
+        }
     }
 
     private void ShowFailedBuyingResult()
     {
+        buttonImage.DOColor(disabledColor, scaleAnimationDuration / 2).OnComplete(() =>
+        {
+            buttonImage.DOColor(Color.white, scaleAnimationDuration / 2);
+        });
+        buttonImage.transform.DOPunchScale(scaleAnimationPunchVector, scaleAnimationDuration, scaleAnimationFrequency).OnComplete(() =>
+        {
+            buttonActivated = false;
+        });
+    }
 
+    private void ShowSuccessfullBuyingResult()
+    {
+        buttonActivated = false;
+        ShowAnimation_ButtonPressed();
     }
 }
