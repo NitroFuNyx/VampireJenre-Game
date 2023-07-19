345abd35cf00b559b344003c2859351d10218819
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-07-19 17:47:15 +0300

    Change level map system added

---
+++ Assets/Scripts/Global Controllers/MapsManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapsManager : MonoBehaviour
+{
+    [Header("Current Map Data")]
+    [Space]
+    [SerializeField] private LevelMaps currentLevelMap;
+
+    private List<LevelMaps> levelMapsList = new List<LevelMaps>();
+
+    public event Action<LevelMaps> OnMapChanged;
+
+    private void Awake()
+    {
+        levelMapsList.Add(LevelMaps.Cementary);
+        levelMapsList.Add(LevelMaps.Castle);
+    }
+
+    private void Start()
+    {
+        SetLevelMap(LevelMaps.Cementary);
+    }
+
+    public void PressButton_ChangeMap(SelectionArrowTypes buttonArrowType)
+    {
+        int newMapIndex = levelMapsList.IndexOf(currentLevelMap);
+
+        if (buttonArrowType == SelectionArrowTypes.Right)
+        {
+            newMapIndex += 1;
+
+            if (newMapIndex == levelMapsList.Count)
+            {
+                newMapIndex = 0;
+            }
+        }
+        else
+        {
+            newMapIndex -= 1;
+
+            if (newMapIndex < 0)
+            {
+                newMapIndex = levelMapsList.Count - 1;
+            }
+        }
+
+        SetLevelMap(levelMapsList[newMapIndex]);
+    }
+
+    private void SetLevelMap(LevelMaps map)
+    {
+        currentLevelMap = map;
+        OnMapChanged?.Invoke(currentLevelMap);
+    }
+}
